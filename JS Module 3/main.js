1a615a65cc37b6f5921c3a79bc55e174aa1689ba
bogdandey58 <bogdandey58@gmail.com>
2018-04-27 10:06:31 +0300

    Add files via upload

---
+++ JS Module 3/main.js
@@ -0,0 +1,16 @@
+const key = [
+    'qwertyuiop[]', //0
+    'asdfghjkl;\'', //1
+    'zxcvbnm,./' //2
+];
+
+let hello = key[1][5] + key[0][2] + key[1][8] + key[1][8] + key[0][8];
+
+let javascript = key[1][6] + key[1][0] + key[2][3] + key[1][0] + key[1][1] + key[2][2] + key[0][3] + key[0][7] + key[0][9] + key[0][4];
+
+let trainer = key[0][4] + key[0][3] + key[1][0] + key[0][7] + key[2][5] + key[0][2] + key[0][4];
+
+
+console.log('hello');
+console.log('javascript');
+console.log('trainer');
